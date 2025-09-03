55a3044b4a27a118189923dc33d9ab1aeaae8e4a
tim Ren <timren@139-132-6-26.eduroam-public.deakin.edu.au>
2025-09-03 17:44:56 +1000

    refactor: create discriminated union types for pricing rules

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,21 @@ export interface ProductType {
     name: string;
     price: number;
 }
+
+export interface BuyXGetYFreeType {
+    name: "buyXGetYFree";
+    product: ProductType;
+    buy: number;
+    free: number;
+    quantity: number;
+}
+export interface BulkDiscountType {
+    name: "bulkDiscount";
+    product: ProductType;
+    quantity: number;
+    quantity_threshold: number;
+    discount_price: number;
+}
+
+export type PricingRuleType = BuyXGetYFreeType | BulkDiscountType;
+
